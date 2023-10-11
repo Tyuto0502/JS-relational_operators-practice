313ecdd475dabd13516fdbafd59ed6bfbb0334a1
Yuto Tamura <cayt1224@gmail.com>
2023-10-11 21:23:23 +0900

    課題作成

---
+++ main.js
@@ -0,0 +1,23 @@
+var x = 7;
+var y = 4;
+if (x > y) {
+  console.log("xはyより大きい");
+}
+
+var x = 3;
+var y = 5;
+if (x < y) {
+  console.log("xはyより小さい");
+}
+
+var x = 7;
+var y = 4;
+if (x >= y) {
+  console.log("xはy以上");
+}
+
+var x = 4;
+var y = 4;
+if (x <= y) {
+  console.log("xはy以下");
+}
